1f99924901ea737837edd4d86c42f48473ade7ca
pattirukmangada <20am1a3128@svrec.ac.in>
2026-02-13 21:09:11 +0530

    update backend

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -6,7 +6,7 @@
  */
 
 header('Content-Type: application/json; charset=utf-8');
-header('Access-Control-Allow-Origin: ' . (getenv('FRONTEND_URL') ?: 'http://localhost:5173'));
+header('Access-Control-Allow-Origin: ' . (getenv('FRONTEND_URL') ?: 'https://matrimony.rukmantech.com'));
 header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
 header('Access-Control-Allow-Headers: Content-Type, Authorization');
 header('Access-Control-Max-Age: 3600');
